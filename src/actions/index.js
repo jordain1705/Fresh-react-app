86a459f80bad5e6077b74b06ac26188d6e228578
Jordain Gijsbertha <jordain1997@hotmail.com>
2017-10-19 14:59:08 +0200

    bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,7 @@
 export const selectUser = (user) => {
-
-  //logic
-  console.log("you clicked on user", user.first);
-
-  return {
-    type: "USER_SELECTED",
-    payload: user
-  }
+    console.log("You clicked on user: ", user.first);
+    return {
+        type: 'USER_SELECTED',
+        payload: user
+    }
 };
