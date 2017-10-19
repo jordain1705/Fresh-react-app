b2dfab4d3bea6d22dd4f61a758335975b12172e6
Jordain Gijsbertha <jordain1997@hotmail.com>
2017-10-19 14:53:07 +0200

    Reducer
    Luisterd voor een action and recieves it met een payload.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import {combineReducers} from 'redux';
 import UserReducer from './reducer_user';
-
+import ActiveUserReducer from './reducer_active_user';
 
 
 //colects the Reducers and combine's them into one object called allReducers
 const allReducers = combineReducers({
-      users: UserReducer
-
+      users: UserReducer,
+      activeUser: ActiveUserReducer
      });
 
 export default allReducers;
---
+++ src/reducers/reducer_active_user.js
@@ -0,0 +1,11 @@
+// its job is to listen for an action.
+export default function(state = null, action) {
+
+  switch (action.type) {
+    case USER_SELECTED:
+      return action.payload;
+      break;
+
+  }
+  return state;
+}
