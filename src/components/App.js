cbe3cc8dd4e3dc6e2bb77578ccca17d67f886352
Jordain Gijsbertha <jordain1997@hotmail.com>
2017-10-19 14:19:53 +0200

    containers
    dit is een component met een brain. Het haalt data op van je store .

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import UserList from '../containers/user_list';
 
 
 const App = () => (
 
   <div>
     <h2>Username List:</h2>
+    <UserList/>
     <hr/>
     <h2>User Detail:</h2>
   </div>
